268aa7c029060cbbc38d50c82cddaf283fdb7f53
Lais-coder <laisdaguerra@gmail.com>
2025-10-14 16:08:34 -0300

    index.tsx file adjustment

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,5 +1,11 @@
+import { Sidebar } from '../components/sidebar';
 export function Home() {
   return (
-    <div>Home Page</div>
+    <div className="flex">
+      <Sidebar />
+      <main className="flex-1 p-6">
+        <h1 className="text-2xl font-bold">Home Page</h1>
+      </main>
+    </div>
   );
 }
